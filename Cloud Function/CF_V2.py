ddd89240e5ec882671a8778683cc466deb003246
Muhammad Shahid <ms_shahid@outlook.com>
2024-02-07 13:20:39 +0000

    Adding Code for Cloud function.

---
+++ Cloud Function/CF_V2.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from pandas.io import gbq
+from google.cloud import bigquery
+
+'''
+Python libraries to be installed
+
+gcsfs
+fsspec
+pandas
+pandas-gbq
+
+'''
+
+
+def gcs_to_bigQuery(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context:  Metadata for the event.
+    """
+    # Create a list to store information about the event
+    lst = []
+    file_name = event['name']
+    table_name = file_name.split('.')[0]
+    # Step 1:  Define a dictionary containing info about metadata about the event
+    dct = {
+        'Event_ID': context.event_id,
+        'Event_type': context.event_type,
+        'Bucket_name': event['bucket'],
+        'File_name': event['name'],
+        'Created': event['timeCreated'],
+        'Updated': event['updated']
+    }
+    lst.append(dct)
+    # Step 2: Read the data from the dictionary into pandas data frame.
+    df_metadata = pd.DataFrame.from_records(lst)
+    # Step 3: Write the data into BigQuery.
+    df_metadata.to_gbq('gcp_ms_demo.data_loading_metadata', # Change the dataset and table name
+                       project_id='gcp_ms_demo-123124', # Change project_id
+                       if_exists='append',
+                       location='us')
+    # Let's now write our CSV file to BigQuery
+
+    # Step 1: Read the file into pandas data frame
+    df_data = pd.read_csv('gs://' + event['bucket'] + '/' + file_name)
+
+    # Step 2: Loading the data into BigQuery
+    df_data.to_gbq('gcp_dataeng_demos.' + table_name, # Change this
+                   project_id='gcp-dataeng-demos-365206', # Change this
+                   if_exists='append',
+                   location='us')
---
+++ Cloud Function/CLF.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pandas.io import gbq
+from google.cloud import bigquery
+
+'''
+Python libraries to be installed
+
+gcsfs
+fsspec
+pandas
+pandas-gbq
+
+'''
+
+
+def gcs_to_bigQuery(event,context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+    """
+    # Step 0: Gathering the event info
+    file_name = event['name']
+    table_name = file_name.split('.')[0]
+    bucket_name = event['bucket']
+
+    # Loading the data into BigQuery using Pandas
+
+    # Step 1: Read our csv file into BigQuery
+    df_data = pd.read_csv('gs://' + bucket_name + '/' + file_name)
+    # Step 2: Write our csv file into BigQuery
+    df_data.to_gbq('gcp_ms_demo.' + table_name, # Change the table name.
+                   project_id='gcp_ms_demo-demos-123242', ## Change the project ID
+                   if_exists='append',
+                   location='us') ## location of BigQuery instance instance
