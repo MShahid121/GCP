22f52396f78e4315b450d7e2c2fe82b36a724501
Muhammad Shahid <ms_shahid@outlook.com>
2024-02-08 10:14:40 +0000

    Tidy up

--- Cloud Function/Version 1/main.py.py
+++ Cloud Function/Version 1/main.py.py
@@ -13,7 +13,7 @@ pandas-gbq
 '''
 ## The above libraries need to be referenced in the requirement file
 
-def gcs_to_bigQuery(event,context):
+def hello_gcs(event,context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
          event (dict): Event payload.
@@ -28,7 +28,7 @@ def gcs_to_bigQuery(event,context):
     # Step 1: Read our csv file into BigQuery
     df_data = pd.read_csv('gs://' + bucket_name + '/' + file_name)
     # Step 2: Write our csv file into BigQuery
-    df_data.to_gbq('gcp_ms_demo.' + table_name, # Change the table name.
-                   project_id='gcp_ms_demo-demos-123242', ## Change the project ID
+    df_data.to_gbq('de_demo.' + table_name, # Change the dataset name.
+                   project_id='qwiklabs-gcp-02-e2fbcc49555d', ## Change the project ID
                    if_exists='append',
                    location='us') ## location of BigQuery instance instance
